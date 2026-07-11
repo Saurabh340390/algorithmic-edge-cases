67afb281bba41687d41ba4ef823a9df506b33da5
Saurabh340390 <sv340390@gmail.com>
2026-07-11 15:29:47 +0530

    Create program.py

---
+++ LEETCODE 170/program.py
@@ -0,0 +1,25 @@
+class TwoSum:
+
+    def __init__(self):
+        self.freq = {}
+    def add(self, number: int) -> None:
+        if number in self.freq:
+            self.freq[number] += 1
+        else:
+            self.freq[number] = 1
+
+    def find(self, value: int) -> bool:
+        for num in self.freq:
+            complement = value - num
+            if complement == num:
+                if self.freq[num] > 1:
+                    return True
+            elif complement in self.freq:
+                return True
+        return False 
+
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)
